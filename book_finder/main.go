19513d32f28bd685243e0e67082c649c0f115bc1
Dulan24 <Dulanlokugeegana@gmail.com>
2025-06-24 12:15:21 +0530

    updated model structure for user

--- book_finder/main.go
+++ book_finder/main.go
@@ -15,4 +15,14 @@ func main() {
 	router := gin.New()
 	router.Use(gin.Logger())
 
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port) // Starts the server on :8000 or the value of PORT
+
 }
